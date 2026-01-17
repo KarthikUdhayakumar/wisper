62625fa38eb332fb9c5addc77fb4b8d60294888b
KarthikUdhayakumar <44191256+KarthikUdhayakumar@users.noreply.github.com>
2026-01-18 00:29:39 +0530

    feat: Initialize backend with Express application, Mongoose database connection, and core API routes.

---
+++ backend/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import authRoutes from "./routes/authRoutes";
+import chatRoutes from "./routes/chatRoutes";
+import messageRoutes from "./routes/messageRoutes";
+import userRoutes from "./routes/userRoutes";
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.json({ status: "ok", message: " Backend server is running" });
+})
+
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/user", userRoutes);
+
+export default app;
