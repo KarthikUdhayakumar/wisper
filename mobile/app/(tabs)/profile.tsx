5b7ac3714c94a86abb5499c3ddf2fb1b2c5829d5
KarthikUdhayakumar <44191256+KarthikUdhayakumar@users.noreply.github.com>
2026-01-21 23:36:43 +0530

    feat: Implement Clerk authentication with social login and new app routing structure.

---
+++ mobile/app/(tabs)/profile.tsx
@@ -0,0 +1,23 @@
+import { View, Text, ScrollView, Pressable } from "react-native";
+import React from "react";
+import { useAuth } from "@clerk/clerk-expo";
+
+const ProfileTab = () => {
+  const { signOut } = useAuth();
+  return (
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      className="bg-surface"
+    >
+      <Text className="text-white">ProfileTab</Text>
+      <Pressable
+        onPress={() => signOut()}
+        className="mt-4 bg-red-600 px-4 py-2 rounded-lg"
+      >
+        <Text>Signout</Text>
+      </Pressable>
+    </ScrollView>
+  );
+};
+
+export default ProfileTab;
